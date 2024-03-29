b3c0a8375d0a5059533aeed04be5f9cdbb3ba60a
Code Crafter <kumar@jatanix.com>
2024-03-29 16:37:04 +0530

    ReactRecordingSourceCode

---
+++ src/Components/Greet.js
@@ -0,0 +1,14 @@
+// create greet component in reactjs which displays simple greet message.
+
+import React from 'react';
+
+function Greet () {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default Greet;
+
