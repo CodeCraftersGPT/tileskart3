2afd3227ad3a5271c8e7b4700734f0d7e7cf66c6
Code Crafter <kumar@jatanix.com>
2024-04-03 21:38:36 +0530

    10-useStateObject

--- src/Components/useStateObject.js
+++ src/Components/useStateObject.js
@@ -6,10 +6,15 @@ function User(){
     const empployee = {name:"Ravi",age:21,salary:10000,married:true}
     let [user,setUser] = useState(empployee);
 
+    // function incrementAge(){
+    //     setUser({...user,age:user.age + 1});
+    //  }
+
     return (
         <>
             <h1>user name is {user.name} and age is {user.age} </h1>
             <h1>user salary is {user.salary} and married is {user.married} </h1>
+            {/* <button onClick={incrementAge}>Increment Age</button> */}
         </>
     )
 }
