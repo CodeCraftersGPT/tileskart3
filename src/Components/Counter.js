5412b5f62e73133b7a690fb31c152173c3b2e57b
Code Crafter <kumar@jatanix.com>
2024-04-11 20:29:04 +0530

    20-useEffect

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,16 +1,51 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 function Counter(){
     const [count,setCount] = useState(10);
+    const [name,setName] = useState('kumar');
     const PI = 3.14;
-    let name = 'kumar';
+
+    useEffect(()=>{
+        console.log('component is mounted');
+
+        return ()=>{
+            console.log('component is unmounted');
+        }
+
+    },[]
+    )
+
+    useEffect(()=>{
+        console.log('component is updated for count : '+count);
+        return ()=>{
+            console.log('component is unmounted');
+        }
+    }
+    ,[count]
+    )
+
+    useEffect(()=>{
+        console.log('component is updated for name : '+name);
+    }
+    ,[name]
+    )
+
+    
+    useEffect(()=>{
+        console.log('component is updated for name1 : '+name);
+        console.log('component is updated for count1 : '+count);
+    }
+    ,[name,count]
+    )
+
+
 
     function incrementCount(){
         setCount(count+1);
     }
 
     function updateName(){
-        name = 'vikram';
+        setName('vikram');
     }
     
     return (
@@ -18,8 +53,8 @@ function Counter(){
             <h1>Counter : {count}</h1>
             <h1> Name : {name}</h1>
             <h1>PI : {PI}</h1>  
-            <button onClick={incrementCount}>Increment</button>
-            <button onClick={updateName}>updateName</button>
+            <button style={{ height: '50px', width: '80px', margin: '20px' }} onClick={incrementCount}>Increment</button>
+            <button style={{ height: '50px', width: '90px', margin: '20px' }} onClick={updateName}>updateName</button>
        </>
     )
 }
